880d8b03f5fc30a544d0e37b0130fe5c317f03d5
gsp <gsp@afilias.info>
2020-03-26 15:20:54 -0400

    Playing with covid-19 data

--- days/01-03-datetimes/code/datetime_date.py
+++ days/01-03-datetimes/code/datetime_date.py
@@ -4,36 +4,35 @@ from datetime import datetime
 from datetime import date
 
 datetime.today()
-#datetime.datetime(2018, 2, 19, 14, 38, 52, 133483)
+# datetime.datetime(2018, 2, 19, 14, 38, 52, 133483)
 
 today = datetime.today()
 
-
 type(today)
-#<class 'datetime.datetime'>
+# <class 'datetime.datetime'>
 
 
 todaydate = date.today()
 
 todaydate
-#datetime.date(2018, 2, 19)
+# datetime.date(2018, 2, 19)
 
 type(todaydate)
-#<class 'datetime.date'>
+# <class 'datetime.date'>
 
 todaydate.month
-#2
+# 2
 
 todaydate.year
-#2018
+# 2018
 
 todaydate.day
-#19
+# 19
 
 
 christmas = date(2018, 12, 25)
 christmas
-#datetime.date(2018, 12, 25)
+# datetime.date(2018, 12, 25)
 
 if christmas is not todaydate:
     print("Sorry there are still " + str((christmas - todaydate).days) + " until Christmas!")
---
+++ days/04-06-collections/covid-19.py
@@ -0,0 +1,56 @@
+import csv
+from collections import defaultdict, namedtuple, Counter
+import requests
+from datetime import timedelta, datetime
+
+now = datetime.now()
+yesterday = timedelta(days=1)
+report = now.strftime('%m-%d-%Y.csv')
+report_yesterday = (now - yesterday).strftime('%m-%d-%Y.csv')
+BASE_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + report
+PREVIOUS_BASE_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + report_yesterday
+TMP = '/tmp'
+
+Covid = namedtuple('Covid', 'City Province Confirmed Deaths Recovered')
+
+Covid_data = defaultdict(list)
+
+r = requests.get(BASE_URL)
+if not r:
+    r = requests.get(PREVIOUS_BASE_URL)
+
+with open('/tmp/'+ report, 'wb') as f:
+    f.write(r.content)
+
+
+def create_dict():
+    with open('/tmp/' + report, 'r') as f:
+        for line in csv.DictReader(f):
+            country = line['Country_Region']
+            province = line['Province_State']
+            city = line['Admin2']
+            confirmed = line['Confirmed']
+            deaths = line['Deaths']
+            recovered = line['Recovered']
+            Covid_data[country].append(
+                Covid(Province=province, City=city, Confirmed=confirmed, Deaths=deaths, Recovered=recovered))
+    return Covid_data
+
+
+def data_by_country(country='*', **kwargs):
+    if country == '*':
+        return create_dict()
+    _ = {}
+    for c, d in kwargs.items():
+        if c == country:
+            _[country] = d
+    return _
+
+
+country_data = create_dict()
+data = data_by_country('Poland', **country_data)
+
+for k, v in data.items():
+    for p in v:
+        print(
+            f'Country: {k}, Province: {p.Province}, City: {p.City}, Confirmed: {p.Confirmed}, Deaths: {p.Deaths}, Recovered: {p.Recovered}')
